7288e852087e70afd1579ad2e60a6c625b0b292f
Jon McManus <due@wxwhatever.com>
2020-03-28 16:35:26 +1000

    Update for silverfish.

--- src/main/java/noobanidus/mods/brh/ConfigManager.java
+++ src/main/java/noobanidus/mods/brh/ConfigManager.java
@@ -12,10 +12,20 @@ public class ConfigManager {
   public static ForgeConfigSpec COMMON_CONFIG;
   public static ForgeConfigSpec.DoubleValue WIDTH;
   public static ForgeConfigSpec.DoubleValue HEIGHT;
+  public static ForgeConfigSpec.BooleanValue SILVERFISH;
+  public static ForgeConfigSpec.DoubleValue SILVERFISH_WIDTH;
+  public static ForgeConfigSpec.DoubleValue SILVERFISH_HEIGHT;
 
   static {
+    COMMON_BUILDER.push("rabbit");
     WIDTH = COMMON_BUILDER.comment("width of the rabbit hitbox").defineInRange("width", 0.8, 0.0, 10.0);
     HEIGHT = COMMON_BUILDER.comment("height of the rabbit hitbox").defineInRange("height", 1.0, 0.0, 10.0);
+    COMMON_BUILDER.pop();
+    COMMON_BUILDER.push("silverfish");
+    SILVERFISH_WIDTH = COMMON_BUILDER.comment("width of the silverfish hitbox").defineInRange("width", 1.0, 0.0, 10.0);
+    SILVERFISH_HEIGHT = COMMON_BUILDER.comment("height of the silverfish hitbox").defineInRange("height", 1.0, 0.0, 10.0);
+    COMMON_BUILDER.pop();
+
     COMMON_CONFIG = COMMON_BUILDER.build();
   }
 
